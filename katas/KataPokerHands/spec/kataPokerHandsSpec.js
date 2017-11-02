4f726caba56fe786eb3dda73b12f44db69d85f1c
Iris <iriscarballo@gmail.com>
2017-11-02 13:17:49 +0100

    End of Kata Poker Hands

--- katas/KataPokerHands/spec/kataPokerHandsSpec.js
+++ katas/KataPokerHands/spec/kataPokerHandsSpec.js
@@ -1,26 +1,113 @@
-describe("KataPokerHands: checking higher card", function() {
-
-  beforeEach(function() {
+describe("checkWinner", function() {
+  it("returns owner of highest card", function() {
+    var white = ["6H", "3D", "KS", "9C", "2D"];
+    var black = ["2C", "3H", "4S", "8C", "AH"];
+    expect(kataPokerHands.checkWinner(white, black)).toEqual(black);
+  });
+  it("returns Tie! in case of draw", function() {
+    var white = ["6H", "3D", "AS", "9C", "5D"];
+    var black = ["6C", "3H", "AS", "9C", "5H"];
+    expect(kataPokerHands.checkWinner(white, black)).toEqual("Tie!");
+  });
+  it("returns owner of highest card recursively", function() {
+    var white = ["6H", "3D", "AS", "9C", "5D"];
+    var black = ["6C", "3H", "AS", "9C", "2H"];
+    expect(kataPokerHands.checkWinner(white, black)).toEqual(white);
   });
 
-  it("Checks that Ace is higher than any number", function() {
-    var aCard = kataPokerHands.card("AH");
-    var otherCard = kataPokerHands.card("4D");
-    expect(kataPokerHands.checkHighestCard(aCard, otherCard)).toEqual(aCard);
+  it("checks that a pair wins over not fitted cards", function() {
+    var white = ["6H", "9D", "AS", "5C", "4D"];
+    var black = ["6C", "3H", "AS", "9C", "3H"];
+    expect(kataPokerHands.checkWinner(white, black)).toEqual(black);
   });
-  it("Checks what is the highest card without symbols", function() {
-    var aCard = kataPokerHands.card("5H");
-    var otherCard = kataPokerHands.card("6D");
-    expect(kataPokerHands.checkHighestCard(aCard, otherCard)).toEqual(otherCard);
+  it("returns owner of highest pair", function() {
+    var white = ["6H", "9D", "AS", "7C", "AD"];
+    var black = ["6C", "3H", "AS", "9C", "3H"];
+    expect(kataPokerHands.checkWinner(white, black)).toEqual(white);
   });
-  it("Checks draw between two cards", function() {
-    var aCard = kataPokerHands.card("5H");
-    var otherCard = kataPokerHands.card("5D");
-    expect(kataPokerHands.checkHighestCard(aCard, otherCard)).toEqual("Tie!");
+
+  it("checks that two pairs wins over a pair", function() {
+    var white = ["6H", "9D", "AS", "9C", "6D"];
+    var black = ["6C", "AH", "AS", "9C", "3H"];
+    expect(kataPokerHands.checkWinner(white, black)).toEqual(white);
   });
-  it("Checks what is the highest card with symbols", function() {
-    var aCard = kataPokerHands.card("QH");
-    var otherCard = kataPokerHands.card("AD");
-    expect(kataPokerHands.checkHighestCard(aCard, otherCard)).toEqual(otherCard);
+  it("returns owner of highest two pairs", function() {
+    var white = ["6H", "9D", "AS", "9C", "6D"];
+    var black = ["6C", "AH", "AS", "6C", "3H"];
+    expect(kataPokerHands.checkWinner(white, black)).toEqual(black);
+  });
+  it("returns owner of highest card with identical two pairs", function() {
+    var white = ["6H", "9D", "AS", "AC", "6D"];
+    var black = ["6H", "AH", "AS", "6C", "TH"];
+    expect(kataPokerHands.checkWinner(white, black)).toEqual(black);
+  });
+
+  it("checks that Three of a Kind  wins over a pair", function() {
+    var white = ["6H", "9D", "AS", "9C", "6D"];
+    var black = ["6C", "4H", "4S", "4C", "3H"];
+    expect(kataPokerHands.checkWinner(white, black)).toEqual(black);
+  });
+  it("returns owner of highest Three of a Kind", function() {
+    var white = ["6H", "9D", "9S", "9C", "6D"];
+    var black = ["6C", "4H", "4S", "4C", "3H"];
+    expect(kataPokerHands.checkWinner(white, black)).toEqual(white);
+  });
+
+  it("checks that Straight wins over Three of a Kind", function() {
+    var white = ["3C", "4H", "5S", "7C", "6H"];
+    var black = ["6H", "9D", "9S", "9C", "5D"];
+    expect(kataPokerHands.checkWinner(white, black)).toEqual(white);
+  });
+  it("returns owner of highest Straight", function() {
+    var white = ["3C", "4H", "5S", "7C", "6H"];
+    var black = ["6H", "7D", "8S", "9C", "TD"];
+    expect(kataPokerHands.checkWinner(white, black)).toEqual(black);
+  });
+
+  it("checks that Flush wins over Straight", function() {
+    var white = ["3C", "4H", "5S", "7C", "6H"];
+    var black = ["6H", "5H", "2H", "9H", "KH"];
+    expect(kataPokerHands.checkWinner(white, black)).toEqual(black);
+  });
+  it("returns owner of highest Flush", function() {
+    var white = ["3C", "4C", "KC", "7C", "6C"];
+    var black = ["6H", "5H", "2H", "9H", "KH"];
+    expect(kataPokerHands.checkWinner(white, black)).toEqual(black);
+  });
+  it("checks Flush tie", function() {
+    var white = ["5C", "6C", "9C", "2C", "KC"];
+    var black = ["6H", "5H", "2H", "9H", "KH"];
+    expect(kataPokerHands.checkWinner(white, black)).toEqual("Tie!");
+  });
+
+  it("checks that Full House wins over Flush", function() {
+    var white = ["3C", "3H", "7S", "7C", "7H"];
+    var black = ["6H", "5H", "2H", "9H", "KH"];
+    expect(kataPokerHands.checkWinner(white, black)).toEqual(white);
+  });
+  it("returns owner of highest Full House", function() {
+    var white = ["3C", "3H", "3S", "7C", "7S"];
+    var black = ["6H", "6C", "6S", "KS", "KH"];
+    expect(kataPokerHands.checkWinner(white, black)).toEqual(black);
+  });
+  it("checks that Four of a Kind  wins over Full House", function() {
+    var white = ["9H", "9D", "9S", "9C", "6D"];
+    var black = ["6H", "6C", "6S", "KS", "KH"];
+    expect(kataPokerHands.checkWinner(white, black)).toEqual(white);
+  });
+  it("returns owner of highest Four of a kind", function() {
+    var white = ["9H", "9D", "9S", "9C", "6D"];
+    var black = ["KD", "KC", "6S", "KS", "KH"];
+    expect(kataPokerHands.checkWinner(white, black)).toEqual(black);
+  });
+  it("checks that Straight flush  wins over Four of a kind", function() {
+    var white = ["4H", "6H", "5H", "7H", "3H"];
+    var black = ["6H", "6C", "6S", "KS", "KH"];
+    expect(kataPokerHands.checkWinner(white, black)).toEqual(white);
+  });
+  it("returns owner of highest Straight flush", function() {
+    var white = ["4H", "6H", "5H", "7H", "3H"];
+    var black = ["KD", "QD", "JD", "TD", "9D"];
+    expect(kataPokerHands.checkWinner(white, black)).toEqual(black);
   });
 });
--- katas/KataPokerHands/src/kataPokerHands.js
+++ katas/KataPokerHands/src/kataPokerHands.js
@@ -5,18 +5,398 @@ var kataPokerHands = {
     return {
       value: cardName[0],
       suit: cardName[1],
-      puntuationValue: this.VALUES.indexOf(cardName [0])
+      puntuationValue: this.VALUES.indexOf(cardName[0])
     }
   },
 
-  checkHighestCard : function(aCard, otherCard) {
-    if(otherCard.puntuationValue > aCard.puntuationValue){
-      return otherCard;
-    } else if(aCard.puntuationValue > otherCard.puntuationValue) {
-      return aCard;
-    } else {
-      return "Tie!"
+  hand : function(cardList) {
+    var that = this;
+    return {
+      values: function() {
+        var handValues = [];
+        for (var i = 0; i < cardList.length; i++){
+          handValues.push(that.card(cardList[i]).puntuationValue);
+        }
+        return handValues;
+      },
+      sort: function() {
+        function compareNumbers(a, b) {
+          return b - a;
+        }
+        var handSorted = this.values().sort(compareNumbers);
+        return handSorted;
+      },
     }
-  }
+  },
+
+  checkWinner : function(white, black) {
+    var winner;
+    winner = this.checksHighestStaightFlush(white, black);
+    if(winner){
+      return winner;
+    }
+    winner = this.checksHighestFourOfAKind(white, black);
+    if(winner){
+      return winner;
+    }
+    winner = this.checksHightestFullHouse(white, black);
+    if(winner){
+      return winner;
+    }
+    winner = this.winnerFlush(white, black);
+    if(winner){
+      return winner;
+    }
+    winner = this.checksHightestStraight(white, black);
+    if(winner){
+      return winner;
+    }
+    winner = this.checksHighestThreeOfAKind(white, black);
+    if(winner){
+      return winner;
+    }
+    winner = this.checksHighestOfTwoPair(white,black);
+    if(winner){
+      return winner;
+    }
+    winner = this.checksHighestPair(white,black);
+    if(winner){
+      return winner;
+    }
+    winner = this.checksHighestCard(white, black);
+    if (winner){
+      return winner;
+    }
+  },
+
+  checksHighestCard : function(white, black, index){
+    if (!index){
+      index = 0;
+    }
+    if (index >= white.length) {
+      return "Tie!";
+    }
+    var whiteSorted = this.hand(white).sort()[index];
+    var blackSorted = this.hand(black).sort()[index];
+    if (whiteSorted > blackSorted){
+      console.log("White wins with: " + this.VALUES[whiteSorted]);
+      return white;
+    } else if (whiteSorted < blackSorted){
+      console.log("black wins with: " + this.VALUES[blackSorted]);
+      return black;
+    }
+    return this.checksHighestCard(white, black, index + 1);
+  },
+
+
+  checksHighestPair : function(white, black) {
+    if(!this.checksPair(white) && !this.checksPair(black)){
+      return;
+    }
+    var whiteHasPair = this.checksPair(white);
+    var blackHasPair = this.checksPair(black);
+    if(!whiteHasPair){
+      console.log("Black wins with a pair of: " + this.VALUES[this.checksPair(black)]);
+      return black;
+    }
+    if(!blackHasPair){
+      console.log("White wins with a pair of: " + this.VALUES[this.checksPair(white)]);
+      return white;
+    }
+    if(whiteHasPair > blackHasPair) {
+      console.log("White wins with a pair of: " + this.VALUES[this.checksPair(white)]);
+      return white;
+    }
+    if(whiteHasPair < blackHasPair) {
+      console.log("Black wins with a pair of: " + this.VALUES[this.checksPair(black)]);
+      return black;
+    }
+  },
+
+  checksTwoPairs : function(cardList) {
+    var handSorted = this.hand(cardList).sort();
+    var pairs = [];
+    for(var i = 0; i < (handSorted.length - 1); i++) {
+      if(handSorted[i] === handSorted[i + 1]) {
+        pairs.push(handSorted[i]);
+      }
+    }
+    if(pairs.length === 2){
+      return pairs.sort();
+    }
+  },
+  checksHighestOfTwoPair : function(white, black) {
+    if(!this.checksTwoPairs(white) && !this.checksTwoPairs(black)){
+      return;
+    }
+    var whiteHasTwoPairs = this.checksTwoPairs(white);
+    var blackHasTwoPairs = this.checksTwoPairs(black);
+    if(!whiteHasTwoPairs){
+      console.log("Black wins with two pairs.");
+      return black;
+    }
+    if(!blackHasTwoPairs){
+      console.log("White wins with two pairs.");
+      return white;
+    }
+    if(this.compareTwoPairs(whiteHasTwoPairs, blackHasTwoPairs) === whiteHasTwoPairs){
+      return white;
+    }
+    if(this.compareTwoPairs(whiteHasTwoPairs, blackHasTwoPairs) === blackHasTwoPairs){
+      return black;
+    }
+  },
+  compareTwoPairs : function(whitePairs, blackPairs) {
+    var whiteHasTwoPairs = whitePairs.sort();
+    var blackHasTwoPairs = blackPairs.sort();
+    if(whiteHasTwoPairs[0] > blackHasTwoPairs[0]){
+      console.log("White wins with two pairs.");
+      return whitePairs;
+    }
+    if(whiteHasTwoPairs[0] < blackHasTwoPairs[0]){
+      console.log("Black wins with two pairs.");
+      return blackPairs;
+    }
+    if(whiteHasTwoPairs[0] === blackHasTwoPairs[0]){
+      if(whiteHasTwoPairs[1] > blackHasTwoPairs[1]){
+        console.log("White wins with two pairs.");
+        return whitePairs;
+      }
+      if(whiteHasTwoPairs[1] < blackHasTwoPairs[1]){
+        console.log("Black wins with two pairs.");
+        return blackPairs;
+      }
+    }
+  },
+
+  checksFourOfAKind : function(cardList){
+    return this.checkNumberOfRepetitions(cardList, 4);
+  },
+  checksThreeOfAKind : function(cardList) {
+    return this.checkNumberOfRepetitions(cardList, 3);
+  },
+  checksPair : function(cardList) {
+    return this.checkNumberOfRepetitions(cardList, 2);
+  },
+  checksEqualNumbers : function(handRestHand){
+    if(handRestHand[0] === handRestHand[1]){
+      return handRestHand[0];
+    }
+  },
+
+  checkNumberOfRepetitions : function(cardList, repetitions) {
+    var handSorted = this.hand(cardList).sort();
+    var index = repetitions - 1;
+    for(var i = 0; i < (handSorted.length - index); i++) {
+      if(handSorted[i] === handSorted[i + index]) {
+        return handSorted[i];
+      }
+    }
+  },
+
+  checksHighestThreeOfAKind : function(white, black) {
+    if(!this.checksThreeOfAKind(white) && !this.checksThreeOfAKind(black)){
+      return;
+    }
+    var whiteHasThreeOfAKind = this.checksThreeOfAKind(white);
+    var blackHasThreeOfAKind = this.checksThreeOfAKind(black);
+
+    if(!whiteHasThreeOfAKind){
+      console.log("Black wins with a three of a kind of: " + this.VALUES[this.checksThreeOfAKind(black)]);
+      return black;
+    }
+    if(!blackHasThreeOfAKind){
+      console.log("White wins with a three of a kind of: " + this.VALUES[this.checksThreeOfAKind(white)]);
+      return white;
+    }
+    if(whiteHasThreeOfAKind > blackHasThreeOfAKind) {
+      console.log("White wins with a three of a kind of: " + this.VALUES[this.checksThreeOfAKind(white)]);
+      return white;
+    }
+    if(whiteHasThreeOfAKind < blackHasThreeOfAKind) {
+      console.log("Black wins with a three of a kind of: " + this.VALUES[this.checksThreeOfAKind(black)]);
+      return black;
+    }
+  },
+
+  checksHightestStraight : function(white, black){
+    var whiteHasStraight = this.checksStraight(white);
+    var blackHasStraight = this.checksStraight(black);
+
+    if(!whiteHasStraight && !blackHasStraight){
+      return;
+    }
+
+    if(!whiteHasStraight){
+      console.log("Black wins with a Straight");
+      return black;
+    }
+    if(!blackHasStraight){
+      console.log("White wins with a Straight");
+      return white;
+    }
+    if(whiteHasStraight > blackHasStraight) {
+      console.log("White wins with a Straight");
+      return white;
+    }
+    if(whiteHasStraight < blackHasStraight) {
+      console.log("Black wins with a Straight");
+      return black;
+    }
+  },
+
+  checksStraight : function(cardList) {
+    var handSorted = this.hand(cardList).sort();
+    var highestCardOfStraight = handSorted[0];
+    var consecutive = true;
+    for (var i = 0; i < handSorted.length - 1; i++) {
+      if (handSorted[i] !== handSorted[i + 1] + 1) {
+        consecutive = false;
+      }
+    }
+    if(consecutive){
+      return highestCardOfStraight;
+    }
+  },
+
+  winnerFlush : function(white, black) {
+    var whiteHasFlush = this.checksFlush(white);
+    var blackHasFlush = this.checksFlush(black);
+
+    if(!whiteHasFlush && !blackHasFlush){
+      return;
+    }
+    if(whiteHasFlush && blackHasFlush){
+      return this.checksHighestCard(white, black);
+    }
+    if(whiteHasFlush){
+      console.log("White wins with a Flush");
+      return white;
+    }
+    if(blackHasFlush){
+      console.log("Black wins with a Flush");
+      return black;
+    }
+  },
+
+  checksFlush : function(cardList) {
+    var flush = true;
+    for(var i = 0; i < cardList.length - 1; i++){
+      var suit = this.card(cardList[i]).suit;
+      if(suit !== this.card(cardList[i + 1]).suit){
+        flush = false;
+      }
+    }
+    return flush;
+  },
+
+  checksFullHouse(cardList){
+    var handHasFullHouse = [];
+    var handFullThree = this.checksThreeOfAKind(cardList);
+    var handRestHand = this.getRestOfHand(cardList, handFullThree);
+    var handFullPair = this.checksEqualNumbers(handRestHand);
+    if(handFullThree && handFullPair){
+      handHasFullHouse.push(handFullThree, handFullPair);
+      return handHasFullHouse;
+    }
+    return;
+  },
+
+  checksHightestFullHouse : function(white, black){
+    var whiteHasFullHouse = this.checksFullHouse(white);
+    var blackHasFullHouse = this.checksFullHouse(black);
+
+    if(!whiteHasFullHouse && !blackHasFullHouse){
+      return;
+    }
+    if(whiteHasFullHouse && blackHasFullHouse){
+      whiteHasTwoPairs = whiteHasFullHouse;
+      blackHasTwoPairs = blackHasFullHouse;
+      if(this.compareTwoPairs(whiteHasTwoPairs, blackHasTwoPairs) === whiteHasTwoPairs){
+        return white;
+        console.log("White wins with Full House");
+      }
+      if(this.compareTwoPairs(whiteHasTwoPairs, blackHasTwoPairs) === blackHasTwoPairs){
+        return black;
+        console.log("Black wins with Full House");
+      }
+    }
+    if(whiteHasFullHouse){
+      console.log("White wins with a Full House");
+      return white;
+    }
+    if(blackHasFullHouse){
+      console.log("Black wins with a Full House");
+      return black;
+    }
+
+  },
+
+  getRestOfHand : function(cardList, toRemove) {
+    var restOfHand = [];
+    cardList = this.hand(cardList).values();
+    for(var i = 0; i < cardList.length; i++){
+        if(toRemove !== cardList[i]){
+        restOfHand.push(cardList[i]);
+      }
+    }
+    return restOfHand;
+  },
+  checksHighestFourOfAKind : function(white, black) {
+    var whiteHasFourOfAKind = this.checksFourOfAKind(white);
+    var blackHasFourOfAKind = this.checksFourOfAKind(black);
+
+    if(!whiteHasFourOfAKind && !blackHasFourOfAKind){
+      return;
+    }
+    if(!whiteHasFourOfAKind){
+      console.log("Black wins with a four of a kind of: " + this.VALUES[this.checksFourOfAKind(black)]);
+      return black;
+    }
+    if(!blackHasFourOfAKind){
+      console.log("White wins with a three of a kind of: " + this.VALUES[this.checksFourOfAKind(white)]);
+      return white;
+    }
+    if(whiteHasFourOfAKind > blackHasFourOfAKind) {
+      console.log("White wins with a three of a kind of: " + this.VALUES[this.checksFourOfAKind(white)]);
+      return white;
+    }
+    if(whiteHasFourOfAKind < blackHasFourOfAKind) {
+      console.log("Black wins with a three of a kind of: " + this.VALUES[this.checksFourOfAKind(black)]);
+      return black;
+    }
+  },
+
+  checksStraightFlush : function(cardList) {
+    if(this.checksFlush(cardList) && this.checksStraight(cardList)){
+      return this.checksStraight(cardList);
+    }
+    return
+  },
+
+  checksHighestStaightFlush : function(white, black) {
+    var whiteStraightFlush = this.checksStraightFlush(white);
+    var blackStraightFlush = this.checksStraightFlush(black);
+
+    if(!whiteStraightFlush && !blackStraightFlush){
+      return;
+    }
+    if(!whiteStraightFlush){
+      console.log("Black wins with Straight Flush.");
+      return black;
+    }
+    if(!blackStraightFlush){
+      console.log("White wins with Straight Flush.");
+      return white;
+    }
+    if(whiteStraightFlush > blackStraightFlush) {
+      console.log("White wins with Straight Flush.");
+      return white;
+    }
+    if(whiteStraightFlush < blackStraightFlush) {
+      console.log("Black wins with Straight Flush");
+      return black;
+    }
+    },
 
 }
